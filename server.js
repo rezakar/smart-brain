d3b69ef92e00e4dab909fde7df00341b6276162f
REZA <hamidmick@yahoo.com>
2020-10-26 14:58:00 -0400

    Updating database

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors());
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : '(postgresql-crystalline-93942',
     user : 'postgres',
     password : 'Hamid368368',
     database : 'smartbrain'
