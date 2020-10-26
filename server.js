d4156c899c88ff5a89449cd7f486640c6898d605
REZA <hamidmick@yahoo.com>
2020-10-26 13:28:30 -0400

    Adding port

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 // end point for clarifai
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
-app.listen(3000, () => {
-    console.log('App runnig');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`App runnig ${process.env.PORT}`);
 })
 
